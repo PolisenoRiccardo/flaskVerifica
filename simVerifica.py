6fdd35f304a562c8fe2a3d4ed7680e8da5db6dc1
Poliseno Riccardo <poliseno.riccardo@iisgalvanimi.edu.it>
2023-03-13 12:09:28 +0000

    commit before morte

--- simVerifica.py
+++ simVerifica.py
@@ -1,12 +1,32 @@
-from flask import Flask, render_template
 import datetime
-now = datetime.datetime.now()
-
+from flask import Flask, render_template
 app = Flask(__name__)
+
 
 @app.route('/')
 def homepage():
-  return render_template("index.html", ora=(now.hour, now.minute, now.second))
+  now = datetime.datetime.now()
+  return render_template("index.html", giorno=now.day, mese=now.month, anno=now.year, ora=now.hour, minuti=now.minute)
+
+@app.route('/mappa')
+def map():
+  return render_template("mappa.html")
+
+@app.route('/mappa600')
+def map1():
+  return render_template("mappa.html", width=600)
+
+@app.route('/mappa800')
+def map2():
+  return render_template("mappa.html", width=800)
+
+@app.route('/mappa1000')
+def map3():
+  return render_template("mappa.html", width=1000)
+  
+@app.route('/template')
+def template():
+  return render_template("Arsha/index.html")
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
